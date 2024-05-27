36c9a3a32fa2facbbe06a81778ff6d79ade12ed8
Sebastián Gallegos F. <59512190+SebaGallegos@users.noreply.github.com>
2024-05-27 10:13:14 -0400

    Inicio de conversión de órdenes de AR a sql

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
 import { SQLiteProvider } from "expo-sqlite";
-import { Text, View } from "react-native";
+import {TextInput, View} from "react-native";
 
 import styles from "./styles/styles.js";
 import Select from "./components/operations/Select.jsx";
 import Union from "./components/operations/Union.jsx";
+import RAtoSQL from "./RAtoSQL";
+
+import {useState} from "react";
 
 export default function App() {
+    const [texto, setTexto] = useState('')
+
+    const handleSubmit = () => {
+        if (texto.trim() === ''){
+            return;
+        }
+        console.log(texto);
+        setTexto('')
+    }
+
   return (
     <View style={styles.container}>
       <SQLiteProvider
-        databaseName="estudiantes.db"
-        assetSource={{ assetId: require("./database/estudiantes.db") }}
+        databaseName="datos.db"
+        assetSource={{ assetId: require("./database/datos.db") }}
         >
-        <Select tabla={"estudiantes_BD"} />
+        <Select tabla={"tabla1"} />
       </SQLiteProvider>
+
+        <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            onChangeText={texto => setTexto(texto)}
+            value={texto}
+            onSubmitEditing={() => {
+             RAtoSQL(texto);
+             handleSubmit()
+            }}
+            blurOnSubmit={false}
+        />
 
       {/* <SQLiteProvider
         databaseName="estudiantes.db"
