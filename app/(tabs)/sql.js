ae37aaa0bbbefeff14f6af9dd483d72bff740e92
Sebastián Gallegos F. <59512190+SebaGallegos@users.noreply.github.com>
2024-06-27 16:05:42 -0400

    Pestaña para ejecutar consultas sql

--- app/(tabs)/sql.js
+++ app/(tabs)/sql.js
@@ -1,6 +1,26 @@
 import { View, Text } from "react-native";
 import styles from "../../styles/estilos";
+import { Button, TextInput } from "react-native-paper";
+import { colores } from "../../styles/colores";
 
 export default function Tab() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={{ flex: 1 }}>
+      <TextInput
+        style={{
+          flex: 1,
+          width: "100%",
+        }}
+      />
+      <View style={styles.container}>
+        <Button
+          style={styles.Button}
+          mode={"contained"}
+          buttonColor={colores.colors.primary}
+        >
+          Enviar
+        </Button>
+      </View>
+    </View>
+  );
 }
